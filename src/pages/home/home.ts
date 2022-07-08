48b6cea806bfb386c7ed1e6481f62a8207d257cf
Godoy <renanfgfg@gmail.com>
2022-07-08 15:03:58 -0300

    local storage armazenando valor token usuario

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -24,7 +24,9 @@ export class HomePage {
 
   public login(){
     this.auth.authenticate(this.creds).subscribe(response=>{
-      console.log(response.headers.get('Authorization'));
+      
+      this.auth.successfulLogin(response.headers.get('Authorization'));
+
       this.navCtrl.setRoot('CategoriasPage');
     }, errror => {
       
